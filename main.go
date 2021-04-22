c60c6345349d02e420b6c3c01e74b9cf7d5e5bab
Richard <richard.yang.tong@gmail.com>
2021-04-22 17:34:21 -0400

    Added some more cases on else if.

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ func main() {
 	myNum := 100
 	isTrue := false
 
-	if myNum > 99 && isTrue {
+	if myNum > 99 && !isTrue {
 		log.Println("myNum is greater than 99 and isTrue is set to true")
+	} else if myNum < 100 && isTrue {
+		log.Println("myNum is less than 99 and isTrue is set to true")
+	} else if myNum == 101 || isTrue {
+		log.Println("myNum is equal to 101 and isTrue is set to true")
 	}
 }
