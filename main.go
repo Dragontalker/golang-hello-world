146e9aa75beed782e0bcbd5de02590d76eda2373
Richard <richard.yang.tong@gmail.com>
2021-04-22 17:25:36 -0400

    Added string slices.

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	names := []string{"one", "seven", "fish", "cat"}
 
-	log.Println(numbers[0:2])
+	log.Println(names)
 }
