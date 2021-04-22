6efa7c1d0b262459c86c6f45113a4465c0aebf2d
Richard <richard.yang.tong@gmail.com>
2021-04-22 15:40:18 -0400

    Added another example for scope.

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ func main() {
 
 	log.Println("s is", s)
 	log.Println("s2 is", s2)
+
+	saySomething("xxx")
 }
 
 func saySomething(s string) (string, string) {
+	log.Println("s from saySomething is ", s)
 	return s, "World"
 }
