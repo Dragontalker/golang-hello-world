b972d4f25410a52285cfb8fc2d337178a9f32dcf
Richard <richard.yang.tong@gmail.com>
2021-04-22 15:16:29 -0400

    Declared a variable.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(saySomething("Hello, world!"))
+	var whatToSay string
+	whatToSay = saySomething("Hello, world!")
+	fmt.Println(whatToSay)
 }
 
 func saySomething(s string) string {
