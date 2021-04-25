8d0825f215798e0769bf73da92ef24b21a600b34
Richard <richard.yang.tong@gmail.com>
2021-04-24 21:51:54 -0400

    Added a second case.

--- Chapter_03/demo_02/main.go
+++ Chapter_03/demo_02/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// case 1: default value is 0
-	var i int
+	// case 1: default value of i is 0
+	// var i int
+
+	// case 2: type inference
+	var i = 10.11
 	fmt.Println("i =", i)
 }
